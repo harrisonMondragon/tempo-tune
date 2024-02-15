48ae3bbcac15bffd56a633c2365f90c56935579e
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-02-15 12:32:25 -0700

    Fix pkce auth example code
    Added gitignore and clientId and redirectURL

--- authorization_code_pkce/public/app.js
+++ authorization_code_pkce/public/app.js
@@ -7,8 +7,8 @@
  * https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
  */
 
-const clientId = 'yourClientIDGoesHere'; // your clientId
-const redirectUrl = 'eg:http://localhost:8080';        // your redirect URL - must be localhost URL and/or HTTPS
+const clientId = '7334ee27ab374209b37f25cfe28c3725'; // your clientId
+const redirectUrl = 'http://localhost:8080';        // your redirect URL - must be localhost URL and/or HTTPS
 
 const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
