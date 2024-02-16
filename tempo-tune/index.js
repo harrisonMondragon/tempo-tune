f52a9897c62da674ad9b91c499149d0f0f4f8395
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-02-16 14:53:56 -0700

    Started using tutorial
    Realized you cant use the get token endpoint from a client, so it is
    impossible to safely perform auth with just react. The spotify-react
    example does a cheesy way of bypassing the real auth by parsing the url,
    but this is unsafe, and I want proper routing. This means I need an actual
    backend. Looked into django and express. Came across tutorial mentioned
    in notes.txt and started following it. I think I'll do the whole thing
    up to deployment, add the actual tempo-tune functionality, maybe tweak
    it a bit, then deploy it. All the old examples are still there, I'll
    remove them eventually. My initial plain react attempt is in
    tempo-tune-old and I'll remove that eventually too

---
+++ tempo-tune/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+// Start the server with [node index.js]
+
+// Demonstrate the using the res parameter
+// Try starting the server
+// Then visiting [http://localhost:8888/]
+// To see the json response with name and isAwesome
+app.get("/", (req, res) => {
+    // res.send("Hello World!");
+    const data = {
+        name: "Harra",
+        isAwesome: true
+    };
+
+    res.json(data);
+});
+
+// Demonstrate the using the req parameter
+// Try starting the server
+// Then visiting [http://localhost:8888/awesome-generator?name=Harra&isAwesome=false]
+// To see "Harra is not awesome"
+// Or [http://localhost:8888/awesome-generator?name=Bits&isAwesome=true]
+// To see "Bits is really awesome"
+app.get('/awesome-generator', (req, res) => {
+    const { name, isAwesome } = req.query;
+    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
+});
+
+// Listen on port 8888
+const port = 8888;
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
+});
--- tempo-tune/src/scripts/fetch.js
+++
@@ -1,9 +0,0 @@
-// Fetching functions to interact with API
-
-export async function fetchProfile() {
-    const token = localStorage.getItem('access_token');
-    const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
-    return await result.json();
-}
