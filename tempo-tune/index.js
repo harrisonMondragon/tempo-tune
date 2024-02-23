56c68db55fd6cf30b0092560ac4ec8269f20a87b
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-02-22 19:31:14 -0700

    [Tut3] Add the Spotify token auth functionality
    This code correctly redirects to spotify auth in the /login route. After
    login is successful it will redirect back to the /callback route where
    it displays the token response json. This should now work regardless of
    incogito mode but I'm not sure about that yet. Need to add better
    comments and such but I gotta run for now

--- tempo-tune/index.js
+++ tempo-tune/index.js
@@ -1,12 +1,15 @@
-const express = require("express");
+require("dotenv").config(); // Handle the .env file that contains app-specific information
+const express = require("express"); // Back-end framework for building RESTful APIs with Node.js
+const querystring = require("querystring"); // Helps handle query params easier
+const axios = require("axios"); // Help handle POST requests easier
+
 const app = express();
+const port = 8888;
 
-// Start the server with [node index.js]
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
-// Demonstrate the using the res parameter
-// Try starting the server
-// Then visiting [http://localhost:8888/]
-// To see the json response with name and isAwesome
 app.get("/", (req, res) => {
     // res.send("Hello World!");
     const data = {
@@ -17,19 +20,78 @@ app.get("/", (req, res) => {
     res.json(data);
 });
 
-// Demonstrate the using the req parameter
-// Try starting the server
-// Then visiting [http://localhost:8888/awesome-generator?name=Harra&isAwesome=false]
-// To see "Harra is not awesome"
-// Or [http://localhost:8888/awesome-generator?name=Bits&isAwesome=true]
-// To see "Bits is really awesome"
-app.get('/awesome-generator', (req, res) => {
-    const { name, isAwesome } = req.query;
-    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
+app.get("/login", (req, res) => {
+
+    // Set a cookie to keep our randomly generated state
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    const scope = 'user-read-private user-read-email';
+
+    // queryParams just helps handle query url parameters easier
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: "code",
+        redirect_uri: REDIRECT_URI,
+        state: state,
+        scope: scope
+    });
+
+    // queryParams evaluates to something like client_id=abc123&response_type=code&redirect_uri=http://localhost:8888/callback
+    // Without using queryParams, the code would be something like:
+    // res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
+
+    // When this URL is hit, it will automatically redirect to Spotify account service URL
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
-// Listen on port 8888
-const port = 8888;
+app.get('/callback', (req, res) => {
+
+    // Store the value of our authorization code which we got from the code query param
+    const code = req.query.code || null;
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+        },
+    })
+    .then(response => {
+        if (response.status === 200) {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        } else {
+            res.send(response);
+        }
+    })
+    .catch(error => {
+        res.send(error);
+    });
+});
+
+// Listens on port 8888
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
 });
